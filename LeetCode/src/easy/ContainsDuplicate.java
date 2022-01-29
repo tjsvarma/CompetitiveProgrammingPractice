5c4453013d6236d3054fb44722388895ff14eb31
Tejas Varma <tjsvarma@gmail.com>
2022-01-29 20:26:59 +0530

    Version 1

---
+++ LeetCode/src/easy/ContainsDuplicate.java
@@ -0,0 +1,36 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ *
+ * Constraints:
+ *
+ *     1 <= nums.length <= 105
+ *     -109 <= nums[i] <= 109
+ *
+ * Time Complexity O(n)
+ */
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i : nums) {
+            if (!(hashSet.add(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ LeetCode/src/easy/MaximumSubarray.java
@@ -0,0 +1,40 @@
+package easy;
+
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ *
+ * A subarray is a contiguous part of an array.
+ *
+ * Example 1:
+ *
+ *  Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ *  Output: 6
+ *  Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ * Constraints:
+ *
+ *     1 <= nums.length <= 105
+ *     -104 <= nums[i] <= 104
+ *
+ * Time Complexity is O(n)
+ *
+ * Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int largestSum = -10000;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            largestSum = (sum > largestSum) ? sum : largestSum;
+            sum = (sum < 0) ? 0 : sum;
+        }
+        return largestSum;
+    }
+}
